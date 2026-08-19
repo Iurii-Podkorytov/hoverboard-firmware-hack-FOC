b8bbb5d2f224411e92b4b72da0104ff571200a66
Iurii-Podkorytov <i.podkorytov@innopolis.university>
2026-08-19 14:14:50 +0300

    Disable motor diagnostics flag

--- Inc/config.h
+++ Inc/config.h
@@ -161,7 +161,7 @@
 // integral term is the culprit.
 #define CTRL_TYP_SEL    FOC_CTRL        // [-] Control type selection: COM_CTRL, SIN_CTRL, FOC_CTRL (default)
 #define CTRL_MOD_REQ    SPD_MODE        // [-] Control mode request: OPEN_MODE, VLT_MODE (default), SPD_MODE, TRQ_MODE. Note: SPD_MODE and TRQ_MODE are only available for CTRL_FOC!
-#define DIAG_ENA        1               // [-] Motor Diagnostics enable flag: 0 = Disabled, 1 = Enabled (default)
+#define DIAG_ENA        0               // [-] Motor Diagnostics enable flag: 0 = Disabled, 1 = Enabled (default)
 
 // Limitation settings
 // Bench-test build: reduced limits for initial power-up validation.
